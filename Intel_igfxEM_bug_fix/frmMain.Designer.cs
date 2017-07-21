cada834dd012ff7eaa8294549e0bcad93ac10d87
JaRam <gitjaram@nerdene.com>
2017-07-21 18:54:59 +0200

    Add project files.

---
+++ Intel_igfxEM_bug_fix/frmMain.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Intel_igfxEM_bug_fix
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnRunSteps = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblStatusService = new System.Windows.Forms.Label();
+            this.lblStatusProcess = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.notIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRunSteps
+            // 
+            this.btnRunSteps.Location = new System.Drawing.Point(383, 226);
+            this.btnRunSteps.Name = "btnRunSteps";
+            this.btnRunSteps.Size = new System.Drawing.Size(75, 23);
+            this.btnRunSteps.TabIndex = 0;
+            this.btnRunSteps.Text = "Apply fix";
+            this.btnRunSteps.UseVisualStyleBackColor = true;
+            this.btnRunSteps.Click += new System.EventHandler(this.btnRunSteps_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Stop service:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Intel(R) HD Graphics Control Panel Service";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "2. Stop process:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "igfxEM Module (igfxEM.exe)";
+            // 
+            // lblStatusService
+            // 
+            this.lblStatusService.Location = new System.Drawing.Point(3, 34);
+            this.lblStatusService.Name = "lblStatusService";
+            this.lblStatusService.Size = new System.Drawing.Size(84, 13);
+            this.lblStatusService.TabIndex = 1;
+            this.lblStatusService.Text = "unknown";
+            this.lblStatusService.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStatusProcess
+            // 
+            this.lblStatusProcess.Location = new System.Drawing.Point(3, 58);
+            this.lblStatusProcess.Name = "lblStatusProcess";
+            this.lblStatusProcess.Size = new System.Drawing.Size(84, 13);
+            this.lblStatusProcess.TabIndex = 1;
+            this.lblStatusProcess.Text = "unknown";
+            this.lblStatusProcess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(118, 84);
+            this.panel1.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 4);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 23);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Step";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(127, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 84);
+            this.panel2.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 4);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(217, 23);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Description";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.lblStatusProcess);
+            this.panel3.Controls.Add(this.lblStatusService);
+            this.panel3.Location = new System.Drawing.Point(356, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(90, 84);
+            this.panel3.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(3, 4);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 23);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Status";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelMain.Controls.Add(this.panel1);
+            this.panelMain.Controls.Add(this.panel3);
+            this.panelMain.Controls.Add(this.panel2);
+            this.panelMain.Location = new System.Drawing.Point(12, 41);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(449, 90);
+            this.panelMain.TabIndex = 2;
+            // 
+            // notIcon
+            // 
+            this.notIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notIcon.Icon")));
+            this.notIcon.Text = "igfxEM bug fix";
+            this.notIcon.Visible = true;
+            this.notIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.showToolStripMenuItem_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click_1);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(15, 226);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(75, 23);
+            this.btnHide.TabIndex = 4;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 261);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.btnRunSteps);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowInTaskbar = false;
+            this.Text = "igfxEM bug fix";
+            this.Activated += new System.EventHandler(this.HideMe);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRunSteps;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblStatusService;
+        private System.Windows.Forms.Label lblStatusProcess;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.NotifyIcon notIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnHide;
+    }
+}
+
---
+++ Intel_igfxEM_bug_fix/frmMain.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using Intel_igfxEM_bug_fix.Properties;
+
+
+namespace Intel_igfxEM_bug_fix
+{
+    public partial class frmMain : Form
+    {
+        private readonly string progPic = Application.StartupPath + "\\Intel_Bug_FIX512x512.png";
+
+        private bool IsHidden = true;
+
+        private int FixRuns = 0;
+        enum ProcStat
+        {
+            Unknown,
+            Starting,
+            Running,
+            Stopping,
+            Stopped
+        }
+
+        private ProcStat _serviceStatus;
+        private ProcStat _processStatus;
+        private ProcStat ServiceStatus
+        {
+            get => _serviceStatus;
+            set
+            {
+                switch (value)
+                {
+                    case ProcStat.Unknown:
+                        lblStatusService.ForeColor = Color.DarkRed;
+                        break;
+                    case ProcStat.Running:
+                        lblStatusService.ForeColor = Color.Red;
+                        break;
+                    case ProcStat.Starting:
+                    case ProcStat.Stopping:
+                        lblStatusService.ForeColor = Color.OrangeRed;
+                        break;
+                    case ProcStat.Stopped:
+                        //run check to see if process also stopped...
+                        lblStatusService.ForeColor = Color.Green;
+                        break;
+                }
+                if (value != _serviceStatus)
+                {
+                    _serviceStatus = value;
+                    lblStatusService.Text = _serviceStatus.ToString();
+                }
+            }
+        }
+        private ProcStat ProcessStatus
+        {
+            get => _processStatus;
+            set
+            {
+                switch (value)
+                {
+                    case ProcStat.Unknown:
+                        lblStatusProcess.ForeColor = Color.DarkRed;
+                        break;
+                    case ProcStat.Running:
+                        lblStatusProcess.ForeColor = Color.Red;
+                        break;
+                    case ProcStat.Starting:
+                    case ProcStat.Stopping:
+                        lblStatusProcess.ForeColor = Color.OrangeRed;
+                        break;
+                    case ProcStat.Stopped:
+                        //run check to see if service also stopped...
+                        lblStatusProcess.ForeColor = Color.Green;
+                        break;
+                }
+                if (value != _processStatus)
+                {
+                    _processStatus = value;
+                    lblStatusProcess.Text = _processStatus.ToString();
+                }
+            }
+        }
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            ServiceStatus = ProcStat.Unknown;
+            ProcessStatus = ProcStat.Unknown;
+            CheckStatus();
+            if (Settings.Default.ApplyFixOnStart)
+            {
+                Run();
+            }
+        }
+
+        private void CheckStatus()
+        {
+            ServiceController sc = new ServiceController();
+            //make variable so servicename can be changed in config
+            sc.ServiceName = Settings.Default.ServiceName;
+            switch (sc.Status)
+            {
+                case ServiceControllerStatus.Running:
+                    ServiceStatus = ProcStat.Running;
+                    break;
+                case ServiceControllerStatus.Stopped:
+                    ServiceStatus = ProcStat.Stopped;
+                    break;
+            }
+            //make variable so processname can be changed in config
+            if (Process.GetProcessesByName(Settings.Default.ProcessName).Length >= 1)
+            {
+                ProcessStatus = ProcStat.Running;
+            }
+            else
+            {
+                ProcessStatus = ProcStat.Stopped;
+            }
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private bool StopService(string serviceName)
+        {
+            bool svar = true;
+            ServiceController sc = new ServiceController();
+            //sc.ServiceName = "igfxCUIservice2.0.0.0";
+            sc.ServiceName = serviceName;
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                try
+                {
+                    // Stop the service, and wait until its status is "Stopped".
+                    sc.Stop();
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped);
+                    
+                    ServiceStatus = ProcStat.Stopped;
+                }
+                catch (InvalidOperationException)
+                {
+                    svar = false;
+                }
+            }
+            return svar;
+        }
+
+        private bool StopProcess(string procName)
+        {
+            bool svar = true;
+            try
+            {
+                //make sure service is stopped...
+                if (StopService("igfxCUIservice2.0.0.0"))
+                {
+                    foreach (var process in Process.GetProcessesByName(procName))
+                    {
+                        process.Kill();
+                    }
+                    ProcessStatus = ProcStat.Stopped;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                svar = false;
+            }
+
+            return svar;
+        }
+
+        private void SetNotText(string text)
+        {
+            notIcon.ShowBalloonTip(5, "Intel bug fix", text, ToolTipIcon.Info);
+        }
+
+        private void btnRunSteps_Click(object sender, EventArgs e)
+        {
+            Run();
+        }
+
+        private void Run()
+        {
+            if (FixRuns < 3)
+            {
+                try
+                {
+                    if (!StopService(Settings.Default.ServiceName))
+                    {
+                        FixRuns++;
+                        Run();
+                    }
+                    if (!StopProcess(Settings.Default.ProcessName))
+                    {
+                        FixRuns++;
+                        Run();
+                    }
+
+                    if (ServiceStatus == ProcStat.Stopped && ProcessStatus == ProcStat.Stopped)
+                    {
+                        SetNotText("Fix applyed...");
+                    }
+                    else
+                    {
+                        FixRuns++;
+                        Run();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fix could not be applyed\nContact JaRam!");
+            }
+            
+        }
+
+        private void showToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            IsHidden = false;
+            this.Show();
+            if (!this.Focused)
+                this.Focus();
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            IsHidden = true;
+            this.Hide();
+        }
+
+        private void HideMe(object sender, EventArgs e)
+        {
+            if (IsHidden)
+            this.Hide();
+        }
+    }
+}
